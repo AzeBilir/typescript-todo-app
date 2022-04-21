a2e6826e91669768c6cb7fb1110818cdee6385ae
Aze Bilir <azebiliraze@gmail.com>
2022-04-22 02:41:48 +0300

    adding singletodo component

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -10,28 +10,27 @@ interface props {
 const InputField: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const InputField = () => {
-    return (
-      <form
-        className="input"
-        onSubmit={(e) => {
-          handleAdd(e);
-          inputRef.current?.blur();
-        }}
-      >
-        <input
-          type="text"
-          placeholder="Enter a Task"
-          value={todo}
-          ref={inputRef}
-          onChange={(e) => setTodo(e.target.value)}
-          className="input__box"
-        />
-        <button type="submit" className="input_submit">
-          GO
-        </button>
-      </form>
-    );
-  };
+  return (
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Enter a Task"
+        value={todo}
+        ref={inputRef}
+        onChange={(e) => setTodo(e.target.value)}
+        className="input__box"
+      />
+      <button type="submit" className="input_submit">
+        GO
+      </button>
+    </form>
+  );
 };
+
 export default InputField;
